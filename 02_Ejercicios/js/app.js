dc3a730d12e50fbef8d8e8d32eee0ba4d7820d6a
LadyDMary <garellanor2400@alumno.ipn.mx>
2026-09-24 18:36:03 -0600

    Se corrigieron errores

--- 02_Ejercicios/js/app.js
+++ 02_Ejercicios/js/app.js
@@ -90,7 +90,7 @@ formObjeto.addEventListener('submit', (evento) =>{
             resultado = JSON.stringify(Object.keys(taller));
             break;
         case 'values':
-            resultado = Object.values(taller);
+            resultado = JSON.stringify(Object.values(taller));
             break;
         case 'entries':
             resultado = Object.entries(taller).map(([campo, valor]) => `${campo}: ${valor}`).join("\n"); 
